d7023743aead1ad8aaab21e645f832dc60da476c
ZzalyOreo <tifansun@gmail.com>
2020-06-01 13:04:50 -0500

    Data init on local todo.service.ts file

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Todo} from '../../models/Todo';
+import {TodoService} from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos',
@@ -9,27 +10,11 @@ import {Todo} from '../../models/Todo';
 export class TodosComponent implements OnInit {
   todos: Todo[];
 
-  constructor() {
+  constructor(private todoService: TodoService) {
   }
 
   ngOnInit(): void {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ];
+    this.todos = this.todoService.getTodos();
   }
 
 }
